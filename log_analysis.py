e963e098eeb3b5c00071463a2010269c18c21d45
Khoi Vu <vuminhkhoi1802@gmail.com>
2019-01-28 22:55:52 +0700

    fixing the source code per reviewer's comments

--- log_analysis.py
+++ log_analysis.py
@@ -1,27 +1,43 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
+
+import os
 
 import psycopg2
-import os
+
+# Import SQL queries from a separate queries file
 from queries import *
 
 db_name = "news"
 text_file = "log_analysis.txt"
 
+
 # Query the database, open & close the Connection to the database
+
+
 def db_query(sql_request):
-	db = psycopg2.connect(database=db_name)
-	c = db.cursor()
-	c.execute(sql_request)
-	results = c.fetchall()
-	db.close()
-	return results
+	try:
+		db = psycopg2.connect(database=db_name)
+		c = db.cursor()
+		c.execute(sql_request)
+		results = c.fetchall()
+		db.close()
+		return results
+	except OSError:
+		print("\n Cannot connect to the database \n")
+		pass
+
 
 # Writing the report
 # Print the title of the report
+
+
 def print_title(title):
 	print('\n\t' + title + '\n')
 
+
 # Remove the current text file before appending new data
+
+
 def remove_text_file(file):
 	try:
 		os.remove(file)
@@ -30,7 +46,10 @@ def remove_text_file(file):
 		print("\n The log_analysis.txt does not exist! \n")
 		pass
 
+
 # Write all the output of queries to a text file
+
+
 def output_to_file(output):
 	f = open('log_analysis.txt', 'a')
 	f.write(output + '\n')
@@ -47,6 +66,7 @@ def top_3_articles():
 		out_top_3 = "{0} -- {1} views".format(title, num)
 		print(out_top_3)
 		output_to_file(out_top_3)
+
 
 # Print and write to file most popular authors
 def the_most_popular_authors():
@@ -67,19 +87,16 @@ def days_errors_display():
 	print_title(title)
 	output_to_file('\n' + title + '\n')
 	for days, error_percentage in days_error:
-		error_day = """{0:%B %d, %Y} -- {1:.3f} % errors""".format(days, error_percentage)
+		error_day = """{0:%B %d, %Y} -- {1:.3f} % errors""" \
+			.format(days, error_percentage)
 		print(error_day)
 		output_to_file(error_day)
 
 
-# Main Function (call al the declared functions)
+# Main Function (call all the declared functions)
 if __name__ == '__main__':
 	remove_text_file(text_file)
 	top_3_articles()
 	the_most_popular_authors()
 	days_errors_display()
 	print("Please refer to log_analysis.txt for the resulting queries")
-
-
-
-
